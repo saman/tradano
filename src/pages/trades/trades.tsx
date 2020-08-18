967c8740b6cf96f85d60e0debaed83f679e58aa0
Saman Soltani <sam.soltani@gmail.com>
2020-08-18 22:57:50 +0200

    add gridjs

--- src/pages/trades/trades.tsx
+++ src/pages/trades/trades.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Grid } from 'gridjs-react';
+import './custom-mermaid.css';
 
 export interface TradesPageProps {
 
 }
 
 const TradesPage: React.SFC<TradesPageProps> = () => {
-    return (<span>Trades</span>);
+    return (
+        <Grid
+            data={[
+                ['John', 'john@example.com'],
+                ['Mike', 'mike@gmail.com']
+            ]}
+            columns={['Name', 'Email']}
+            search={true}
+            pagination={{
+                enabled: true,
+                limit: 1,
+            }}
+        />
+    );
 }
 
 export default TradesPage;
